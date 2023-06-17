7b9f91d9b6c352a9f18d00677f93cb954e451103
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2023-06-17 09:02:56 +0200

    snap scrolling

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,37 +6,57 @@ import { Calculator } from '@/components/calculator/Calculator'
 export default function HomePage() {
   return (
     <main>
-      <AccessibilityBar />
-      <section className="min-h-[50vh] bg-green-100 flex items-center justify-center py-16 px-8">
-        <Container>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold">Kalkulator BAF</h1>
-            <p className="mt-2 mb-4">
-              Twoje ulubione narzędzie do zarządzania zieloną przestrzenią
-            </p>
-            <Button>Oblicz BAF</Button>
+      {/*<AccessibilityBar />*/}
+      {/*<section className="min-h-[50vh] bg-green-100 flex items-center justify-center py-16 px-8">*/}
+      {/*  <Container>*/}
+      {/*    <div className="text-center">*/}
+      {/*      <h1 className="text-4xl font-bold">Kalkulator BAF</h1>*/}
+      {/*      <p className="mt-2 mb-4">*/}
+      {/*        Twoje ulubione narzędzie do zarządzania zieloną przestrzenią*/}
+      {/*      </p>*/}
+      {/*      <Button>Oblicz BAF</Button>*/}
+      {/*    </div>*/}
+      {/*  </Container>*/}
+      {/*</section>*/}
+      {/*<section className="py-16 px-8">*/}
+      {/*  <Container>*/}
+      {/*    <h2 className="text-3xl font-bold">Wpisz numer działki</h2>*/}
+      {/*  </Container>*/}
+      {/*</section>*/}
+      {/*<section className="py-16 px-8">*/}
+      {/*  <Container>*/}
+      {/*    <h2 className="text-3xl font-bold mb-4">Kalkulator</h2>*/}
+      {/*    <Calculator />*/}
+      {/*  </Container>*/}
+      {/*</section>*/}
+      {/*<section className="py-16 px-8">*/}
+      {/*  <Container>*/}
+      {/*    <h2 className="text-3xl font-bold">Zainspiruj się</h2>*/}
+      {/*  </Container>*/}
+      {/*</section>*/}
+      {/*<footer className="py-16 px-8">*/}
+      {/*  <Container>Stopka</Container>*/}
+      {/*</footer>*/}
+      <div className="flex h-screen">
+        <div className="w-1/2 h-screen bg-red-200">lewo</div>
+        <div className="w-1/2 h-screen bg-black snap-y snap-mandatory overflow-y-scroll text-white">
+          <div className="snap-start h-screen text-4xl" id="1">
+            1
           </div>
-        </Container>
-      </section>
-      <section className="py-16 px-8">
-        <Container>
-          <h2 className="text-3xl font-bold">Wpisz numer działki</h2>
-        </Container>
-      </section>
-      <section className="py-16 px-8">
-        <Container>
-          <h2 className="text-3xl font-bold mb-4">Kalkulator</h2>
-          <Calculator />
-        </Container>
-      </section>
-      <section className="py-16 px-8">
-        <Container>
-          <h2 className="text-3xl font-bold">Zainspiruj się</h2>
-        </Container>
-      </section>
-      <footer className="py-16 px-8">
-        <Container>Stopka</Container>
-      </footer>
+          <div className="snap-start h-screen text-4xl" id="2">
+            2
+          </div>
+          <div className="snap-start h-screen text-4xl" id="3">
+            3
+          </div>
+          <div className="snap-start h-screen text-4xl" id="4">
+            4
+          </div>
+          <div className="snap-start h-screen text-4xl" id="5">
+            5
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
